4154aa0f02159e8963b95d15e61536689f8d2467
Ljuba <ljmasirevic@gmail.com>
2017-10-01 13:26:10 +0200

    CreateGoalVC was created

---
+++ CoreDataApp/Controller/CreateGoalVC.swift
@@ -0,0 +1,42 @@
+//
+//  CreateGoalVC.swift
+//  CoreDataApp
+//
+//  Created by Ljubomir on 10/1/17.
+//  Copyright © 2017 Ljubomir. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    
+    @IBAction func nextBtnWasPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func shortTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func longTermBtnWasPressed(_ sender: Any) {
+    }
+    
+    
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
