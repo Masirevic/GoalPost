0e787b362333d79998707c9263329f512c878faa
Ljuba <ljmasirevic@gmail.com>
2017-10-01 17:49:52 +0200

    SavingData Added to App

--- CoreDataApp/Controller/FinishGoalVC.swift
+++ CoreDataApp/Controller/FinishGoalVC.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 class FinishGoalVC: UIViewController, UITextFieldDelegate {
     
@@ -33,9 +34,57 @@ class FinishGoalVC: UIViewController, UITextFieldDelegate {
     }
     
     
-    @IBAction func createGoalBtnWasPressed(_ sender: Any) {
+    @IBAction func backBtnWasPressed(_ sender: Any) {
+        dismisDetail()
     }
     
-   
+    
+    @IBAction func createGoalBtnWasPressed(_ sender: Any) {
+        if pointsTextFields.text != "" {
+            self.save { (complete) in
+                if complete {
+                    dismiss(animated: true, completion: nil)
+                }
+         }
+        
+    }
+        
+}
+    
+    func save (completion: (_ finished: Bool)-> ()) {
+        
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else {return}
+        let goal = Goal(context: managedContext)
+        
+        goal.goalDescription = goalDescription
+        goal.goalType = goalType.rawValue
+        goal.goalCompletionValue = Int32(pointsTextFields.text!)!
+        goal.goalProgress = Int32(0)
+        
+      
+        
+        do {
+            try  managedContext.save()
+            completion(true)
+        } catch {
+        debugPrint("Could not save: \(error.localizedDescription)")
+            completion(false)
+        }
+    }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
